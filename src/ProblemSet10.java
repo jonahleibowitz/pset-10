dd3cdfed5e0176cba74fa3d84409302d51cd926b
Jonah Leibowitz <jonahleibowitz@comcast.net>
2021-02-14 16:06:55 -0500

    finished 2

--- src/ProblemSet10.java
+++ src/ProblemSet10.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ProblemSet10 {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 1, 1, 3};
+        int[] numbers = {1, 1, 1, 1, 1, 7, 1, 3, 1};
        // int end = 20;
         System.out.println(maxSpan(numbers));
         //(Arrays.toString(
@@ -36,16 +36,40 @@ public class ProblemSet10 {
             index ++;
         } }
         return finalArray;
-    /*Plan:
-    * 1, 2, 3, 4, 5, 6 (length = 6) index of 1 is 0 || length- (end)
-    *
-    * 5, 6, 7, 8, 9, 10 (length  = 6) index of 5 is 0 ||
-    * */
-
     }
 
     public static int maxSpan(int[] numbers) {
-
+        int span;
+        int max = 0;
+        if(numbers == null){
+            return -1;
+        } else if (numbers.length == 0){
+            span = 0;
+        } else if (numbers.length == 1){
+            span = 1;
+        } else {
+            int currSpan = 0;
+            for (int i = 0; i < numbers.length; i++) {  //for loop that addresses each value in numbers[]
+                for (int j = i; j < numbers.length; j++) { // loop that starts at the current value in numbers[]
+                    if (numbers[i] == numbers[j]) { // if j matches i, the span becomes distance between
+                        currSpan = j - i + 1;
+                    }
+                }
+                if (currSpan > max) {
+                    max = currSpan;
+                }
+            }
+            span = max;
+        }
+        return span;
+    /*Plan:
+    * Make variable for longest length set to 0
+    * For loop for each value from its index to end of the array:
+    * if values match, its span becomes difference between the two indexes
+    * At the end of loop for the variable, the longest span becomes its final span
+    * if this span is longer than the official span, it becomes the new official
+    * At the end, longest span is returned
+    * */
     }
 /*
     public int[] fix34(int[] numbers) {
